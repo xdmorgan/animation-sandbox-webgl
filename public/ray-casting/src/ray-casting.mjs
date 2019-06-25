a58a8b236f67000d80fe65f91ce1096a2dcacb85
Dan Morgan <hello@danny.codes>
2019-06-24 22:08:38 -0400

    Rotating lights and camera, no casting

--- public/ray-casting/src/ray-casting.mjs
+++ public/ray-casting/src/ray-casting.mjs
@@ -1,17 +1,19 @@
 import { getCanvasDimensions } from "./get-canvas-dimensions.mjs";
 import { randColor, randCoord } from "./random.mjs";
 import { randRotation } from "./random.mjs";
+import { toRadian } from "./convert.mjs";
 
 const { THREE } = window;
-const BOXES = 5000;
+const BOXES = 1000;
+const CAMERA_RADIUS = 60;
+let theta = 0;
 
 export class Scene {
   constructor({ canvas }) {
     this.renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
     // create camera
-    const [fov, aspect, near, far] = [35, 2, 0.1, 100];
+    const [fov, aspect, near, far] = [75, 2, 1, 1000];
     this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    this.camera.position.z = 20;
     // this.controls = new THREE.OrbitControls(
     //   this.camera,
     //   this.renderer.domElement
@@ -20,23 +22,10 @@ export class Scene {
     this.scene = new THREE.Scene(); // a container
     this.scene.background = new THREE.Color();
     // add some lighting
-    const ambient = new THREE.AmbientLight(0xffffff, 0.5);
-    ambient.position.set(0, 0, 50);
-    this.scene.add(ambient);
-    const directional = new THREE.DirectionalLight(0xffffff, 0.5);
-    directional.position.set(0, 0, 50);
-    this.scene.add(directional);
+    this.light = new THREE.DirectionalLight(0xffffff, 1);
+    this.scene.add(this.light);
     // create field of cubes
-    this.objects = [];
-    for (let i = 0; i < BOXES; i++) {
-      const range = [-10, 10];
-      const position = randCoord(range, range, range);
-      const color = randColor();
-      const rotation = randRotation();
-      const cube = new Cube({ position, rotation, color });
-      this.objects.push(cube);
-      this.scene.add(cube.mesh);
-    }
+    for (let i = 0; i < BOXES; i++) this.scene.add(Cube.Create().mesh);
     // raycasting
     this.raycaster = new THREE.Raycaster();
     this.mouse = new THREE.Vector2();
@@ -61,10 +50,23 @@ export class Scene {
       // this.controls.update();
     }
 
-    // raycasting
-    this.raycaster.setFromCamera(this.mouse, this.camera);
-    const intersects = this.raycaster.intersectObjects(this.scene.children);
-    intersects.map(child => child.object.material.color.set(0xff0000));
+    theta += 0.1;
+
+    this.camera.position.x = CAMERA_RADIUS * Math.sin(toRadian(theta));
+    this.camera.position.y = CAMERA_RADIUS * Math.sin(toRadian(theta));
+    this.camera.position.z = CAMERA_RADIUS * Math.cos(toRadian(theta));
+    this.camera.lookAt(this.scene.position);
+    this.light.position.x = CAMERA_RADIUS * Math.sin(toRadian(theta));
+    this.light.position.y = CAMERA_RADIUS * Math.sin(toRadian(theta));
+    this.light.position.z = CAMERA_RADIUS * Math.cos(toRadian(theta));
+    this.light.lookAt(this.scene.position);
+
+    // // raycasting
+    // this.camera.updateMatrixWorld();
+    // this.raycaster.setFromCamera(this.mouse, this.camera);
+    // const intersects = this.raycaster.intersectObjects(this.scene.children);
+    // console.log("intersects", intersects);
+    // intersects.map(child => child.object.material.color.set(0xff0000));
 
     // update and redraw
     this.renderer.render(this.scene, this.camera);
@@ -73,11 +75,18 @@ export class Scene {
 }
 
 class Cube {
+  static Create(field = [-50, 50]) {
+    const position = randCoord(field, field, field);
+    const color = randColor();
+    const rotation = randRotation();
+    return new Cube({ position, rotation, color });
+  }
+
   constructor({
     position = { x: 0, y: 0, z: 0 },
     rotation = { x: 0, y: 0, z: 0 },
     color = "red",
-    size = 0.2
+    size = 1
   }) {
     const geometry = new THREE.BoxBufferGeometry(size, size, size);
     const material = new THREE.MeshLambertMaterial({ color });
